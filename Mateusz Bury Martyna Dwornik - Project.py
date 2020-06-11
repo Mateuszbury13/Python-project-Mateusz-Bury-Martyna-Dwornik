176c4270f702095c36483af0d363c2cc12c1cec2
Matthew <mateuszbury@poczta.onet.pl>
2020-06-11 22:06:05 +0200

    Create Mateusz Bury Martyna Dwornik - Project.py

---
+++ Mateusz Bury Martyna Dwornik - Project.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 11 22:03:58 2020
+
+@author: Mateusz
+"""
+
+
+t = 0
+while t<1:
+  slowo = input("\nPodaj słowo którego odmiane chcesz otrzymać: ")
+  print("\n")
+
+  temp2=slowo
+
+  #odmiana regularna
+  dlugosc = len(slowo)-2
+  #print(dlugosc)
