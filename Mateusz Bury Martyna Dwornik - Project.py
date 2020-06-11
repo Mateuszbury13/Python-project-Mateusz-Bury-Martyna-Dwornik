3f8878f87eac1af3045227f2126e47846eef80fb
Matthew <mateuszbury@poczta.onet.pl>
2020-06-11 22:54:45 +0200

    chn, ffn, kn, tm

--- Mateusz Bury Martyna Dwornik - Project.py
+++ Mateusz Bury Martyna Dwornik - Project.py
@@ -47,3 +47,64 @@ while t<1:
 
     #Sie
     print("Sie",temp2)
+    
+     # t, d
+
+  elif slowo[-3]=='t' or slowo[-3]=='d':
+    #print("3 opcja")
+    #ich
+    slowo = slowo[0:len(slowo)-2]
+    slowo = slowo + "e"
+    print("Ich",slowo)
+
+    # du
+    slowo = slowo[0:len(slowo)-1]
+    slowo = slowo + "est"
+    print("Du",slowo)
+
+    # er/sie/es
+    slowo = slowo[0:len(slowo)-3]
+    slowo = slowo + "et"
+    print("Er / sie / es",slowo)
+
+    #wir
+    print("Wir",temp2)
+
+    # Ihr
+    slowo = slowo[0:len(slowo)-1]
+    temp = "t"
+    ihr = slowo + temp
+    print("Ihr",ihr)
+
+    #Sie
+    print("Sie",temp2)
+    
+     # chn, ffn, kn, tm
+
+  elif (slowo[-4]=='c' and slowo[-3]=='h'and slowo[-2]=='n') or (slowo[-5]=='f' and slowo[-4]=='f' and slowo[-3]=='n') or (slowo[-4]=='k' and slowo[-3]=='n') or (slowo[-4]=='t' and slowo[-3]=='m'):
+    #print("4 opcja")
+    #ich
+    slowo = slowo[:-1]
+    print("Ich",slowo)
+
+    # du
+    slowo = slowo[0:len(slowo)-1]
+    slowo = slowo + "est"
+    print("Du",slowo)
+
+    # er/sie/es
+    slowo = slowo[0:len(slowo)-2]
+    slowo = slowo + "t"
+    print("Er / sie / es",slowo)
+
+    #wir
+    print("Wir",temp2)
+
+    # Ihr
+    slowo = slowo[0:len(slowo)-1]
+    temp = "t"
+    ihr = slowo + temp
+    print("Ihr",ihr)
+
+    #Sie
+    print("Sie",temp2)
