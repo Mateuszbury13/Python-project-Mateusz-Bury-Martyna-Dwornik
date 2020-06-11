f596b4517e1376b8435d528ab90b98b8b7a2bd6f
Matthew <mateuszbury@poczta.onet.pl>
2020-06-11 22:53:09 +0200

    Update Mateusz Bury Martyna Dwornik - Project.py

--- Mateusz Bury Martyna Dwornik - Project.py
+++ Mateusz Bury Martyna Dwornik - Project.py
@@ -16,3 +16,34 @@ while t<1:
   #odmiana regularna
   dlugosc = len(slowo)-2
   #print(dlugosc)
+  
+   #len
+
+  if (slowo[-3]=='e' and slowo[-2]=='l' and slowo[-1]=='n'):
+    #print("2 opcja")
+    #ich
+    slowo = slowo[0:len(slowo)-3]
+    slowo = slowo + "le"
+    print("Ich",slowo)
+
+    # du
+    slowo = slowo[0:len(slowo)-2]
+    slowo = slowo + "elst"
+    print("Du",slowo)
+
+    # er/sie/es
+    slowo = slowo[0:len(slowo)-4]
+    slowo = slowo + "elt"
+    print("Er / sie / es",slowo)
+
+    #wir
+    print("Wir",temp2)
+
+    # Ihr
+    slowo = slowo[0:len(slowo)-1]
+    temp = "t"
+    ihr = slowo + temp
+    print("Ihr",ihr)
+
+    #Sie
+    print("Sie",temp2)
